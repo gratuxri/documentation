8758bd9c270027bf709ac670c8919f1238b4a15f
Niko Wenselowski <n.wenselowski@uib.de>
2017-11-20 17:47:06 +0100

    Link the old documents to the new ones so that links still work.

--- tools/clean_for_opsi4.1.sh
+++ tools/clean_for_opsi4.1.sh
@@ -25,3 +25,18 @@ rm -r html/opsi-v407-releasenotes/
 rm -r html/en/opsi-getting-started/
 rm -r html/en/opsi-manual/
 rm -r html/en/opsi-v407-releasenotes/
+
+# Part 2: Linking the old paths to the new path
+
+ln -s opsi-getting-started-v4.1-de.pdf opsi-getting-started-de.pdf
+ln -s opsi-getting-started-v4.1-en.pdf opsi-getting-started-en.pdf
+ln -s opsi-manual-v4.1-de.pdf opsi-manual-de.pdf
+ln -s opsi-manual-v4.1-en.pdf opsi-manual-en.pdf
+ln -s opsi-getting-started-v4.1-de.epub epub/de/opsi-getting-started-de.epub
+ln -s opsi-getting-started-v4.1-en.epub epub/en/opsi-getting-started-en.epub
+ln -s opsi-manual-v4.1-de.epub epub/de/opsi-manual-de.epub
+ln -s opsi-manual-v4.1-en.epub epub/en/opsi-manual-en.epub
+ln -s opsi-getting-started-v4.1/ html/opsi-getting-started
+ln -s opsi-manual-v4.1/ html/opsi-manual
+ln -s opsi-getting-started-v4.1/ html/en/opsi-getting-started
+ln -s opsi-manual-v4.1/ html/en/opsi-manual

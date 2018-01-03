e155337051fd64e8f100ffdc6f94e07f6ebfa1d6
Niko Wenselowski <n.wenselowski@uib.de>
2018-01-03 16:09:54 +0100

    Add script to clean documentation for opsi 4.0

---
+++ tools/clean_for_opsi4.0.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Run this script from the directory you copied to published files to
+# to remove opsi 4.1 specific documentation.
+
+if [ -e pub.tar ]; then
+    rm pub.tar
+fi
+
+# Removing opsi 4.1 files
+
+rm opsi-getting-started-v4.1-de.pdf
+rm opsi-getting-started-v4.1-en.pdf
+rm opsi-manual-v4.1-de.pdf
+rm opsi-manual-v4.1-en.pdf
+rm epub/de/opsi-getting-started-v4.1-de.epub
+rm epub/en/opsi-getting-started-v4.1-en.epub
+rm epub/de/opsi-manual-v4.1-de.epub
+rm epub/en/opsi-manual-v4.1-en.epub
+rm -r html/opsi-manual-v4.1/
+rm -r html/en/opsi-manual-v4.1/
+rm -r html/opsi-getting-started-v4.1/
+rm -r html/en/opsi-getting-started-v4.1/

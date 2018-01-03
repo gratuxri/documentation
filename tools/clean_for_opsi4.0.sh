ed607e670f34151b3d7dd5d72bf541aaaab2f99c
Niko Wenselowski <n.wenselowski@uib.de>
2018-01-03 16:13:06 +0100

    Making sure no v4.1 releasenotes survive.

--- tools/clean_for_opsi4.0.sh
+++ tools/clean_for_opsi4.0.sh
@@ -13,11 +13,17 @@ rm opsi-getting-started-v4.1-de.pdf
 rm opsi-getting-started-v4.1-en.pdf
 rm opsi-manual-v4.1-de.pdf
 rm opsi-manual-v4.1-en.pdf
+rm opsi-v4.1-releasenotes-de.pdf
+rm opsi-v4.1-releasenotes-en.pdf
 rm epub/de/opsi-getting-started-v4.1-de.epub
 rm epub/en/opsi-getting-started-v4.1-en.epub
 rm epub/de/opsi-manual-v4.1-de.epub
 rm epub/en/opsi-manual-v4.1-en.epub
+rm epub/en/opsi-v4.1-releasenotes-en.epub
+rm epub/de/opsi-v4.1-releasenotes-de.epub
 rm -r html/opsi-manual-v4.1/
 rm -r html/en/opsi-manual-v4.1/
 rm -r html/opsi-getting-started-v4.1/
 rm -r html/en/opsi-getting-started-v4.1/
+rm -r html/opsi-v4.1-releasenotes/
+rm -r html/en/opsi-v4.1-releasenotes
